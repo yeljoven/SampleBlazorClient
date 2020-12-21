a58136a741f0740b0818ce323b87a779a2133d2b
Yel Joven <jovenadiawesome@gmail.com>
2020-12-21 19:30:46 +0800

    Test Bank Rup

---
+++ BankRup/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankRup.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public decimal Balance { get; set; }
+        public string EmailAddress { get; set; }
+        public string ContactNumber { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
---
+++ BankRup/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BankRup.Models
+{
+    public class Transaction
+    {
+        public Guid From { get; set; }
+        public Guid To { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
